f92fedda895bebb48741dc0134d495abdf09fd25
Cody Cooper <Cody-Cooper@users.noreply.github.com>
2022-04-04 01:30:53 -0400

    Update gatsby-config.js
    Update for google analytics (again)

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,25 +7,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-C7HL1TZX28", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
+        trackingIds: ["G-C7HL1TZX28"],
         gtagConfig: {
           optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
+          head: true,
           respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          exclude: ["/preview/**", "/style-guide"],
         },
       },
     },
