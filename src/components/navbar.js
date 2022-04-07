4e6fbf3aacb804efd45abfec52fad38d017fd92a
Cody Cooper <Cody-Cooper@users.noreply.github.com>
2022-04-07 15:03:03 -0400

    update nav bar styling on mobile
    top few pixels of center logo were cut off

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -49,12 +49,12 @@ const NavBar = () => {
           >
             <h1
               sx={{
-                fontSize: ["20px", "40px", "80px"],
+                fontSize: "80px",
                 display: ["none", "none", "inline-block"],
                 background: "var(--theme-ui-colors-twotone)",
                 backgroundClip: "text",
                 color: "transparent",
-                margin: ["37px 0 0 0", "34px 0 0 0", "28px 0 0 0"],
+                margin: "28px 0 0 0",
                 pointerEvents: "auto",
               }}
             >
@@ -65,16 +65,16 @@ const NavBar = () => {
           <Link to="/" sx={{ textDecoration: "none" }}>
             <h1
               sx={{
-                fontSize: ["40px", "80px", "none"],
+                fontSize: "80px",
                 display: ["inline-block", "inline-block", "none"],
                 background: "var(--theme-ui-colors-twotone)",
                 backgroundClip: "text",
                 color: "transparent",
-                margin: ["34px 0 0 0", "28px 0 0 0", "28px 0 0 0"],
+                margin: "28px 0 0 0",
                 pointerEvents: "auto",
               }}
             >
-              CC
+              cc
             </h1>
           </Link>
         </section>
