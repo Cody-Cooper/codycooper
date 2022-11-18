4c07ada7f53cea57f77b4a28c1aa00f9a235c21d
Cody <Cody-Cooper@users.noreply.github.com>
2022-11-17 20:24:18 -0500

    update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,9 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/', title: 'home' },
+  { href: '/blog', title: 'blog' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'about' },
 ]
 
 export default headerNavLinks
