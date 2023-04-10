3e1e3aabb1898d3793bbe171592e831c0361fb19
Cody Cooper <Cody-Cooper@users.noreply.github.com>
2023-04-10 18:58:07 -0400

    Update umami ID (again)

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,7 +21,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '06cf623d-6320-4645-aa8b-a89c2a9053d1', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: '3fc89c7b-57a8-4a9e-a3ae-c163bfa76037', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
