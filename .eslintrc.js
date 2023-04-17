eec9a92ea0e031c7dbdbd8510dd02cff6330ef6b
Cody Cooper <Cody-Cooper@users.noreply.github.com>
2023-04-16 20:38:33 -0400

    update endOfLine in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,16 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
-    'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-key': 'off', // allow commits to happen, problem with key in hamburger and headerNavLinks
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'react/no-unescaped-entities': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
