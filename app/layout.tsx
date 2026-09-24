f725d989dc001f32ec406b65f36e700f68663d87
Cody <Cody-Cooper@users.noreply.github.com>
2026-09-24 03:29:29 -0400

    Remove MailerLite embed script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import Script from "next/script";
 
 import { Analytics } from "@/components/analytics";
 import { ModeToggle } from "@/components/mode-toggle";
@@ -69,13 +68,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
 
           <Analytics />
-          <Script
-            id="mailerlite-universal"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments);},l=d.createElement(e),l.async=1,l.src=u,n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');ml('account', '2656375');`,
-            }}
-          />
         </ThemeProvider>
       </body>
     </html>
