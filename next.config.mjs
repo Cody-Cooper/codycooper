c67390bc87a96349508fc26fdacc474753cd1629
Cody <Cody-Cooper@users.noreply.github.com>
2026-09-26 10:38:24 -0400

    Integrate Content Collections with Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
-import createMDX from "@next/mdx";
+import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [
-      "remark-gfm",
-      "remark-frontmatter",
-      ["remark-mdx-frontmatter", { name: "frontmatter" }],
-    ],
-    rehypePlugins: ["rehype-code-titles", "rehype-prism-plus"],
-  },
-});
-
-export default withMDX(nextConfig);
+export default withContentCollections(nextConfig);
